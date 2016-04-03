ce64615e035465e2d4c0ab6a6becc4f0da55d2ed
vtt01uk <vtt01uk@gmail.com>
2016-04-03 14:14:35 +0100

    Added new and create actions for pictures_controller. Created new view page and generated form partial using Simple_form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,19 @@ class PicturesController < ApplicationController
   
   def index
   end
+  
+  def new
+    @picture = Picture.new
+  end
+  
+  def create
+    @picture = Picture.new(picture_params)
+  end
+  
+  private
+  
+  def picture_params
+    params.require(:picture).permit(:title, :description)
+  end
+  
 end
