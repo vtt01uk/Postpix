060805ff6adc7029b4de89a98f2e0c4594038980
vtt01uk <vtt01uk@gmail.com>
2016-04-03 14:55:07 +0100

    Changed application.html.erb into an haml file. Added show action; now able to display picture posts

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,11 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only: [:show, :edit, :update, :destroy]
   
   def index
+  end
+  
+  def show
+    
   end
   
   def new
@@ -9,12 +14,23 @@ class PicturesController < ApplicationController
   
   def create
     @picture = Picture.new(picture_params)
+
+    if @picture.save
+      flash[:notice] = "Picture saved~"
+      redirect_to @picture
+    else
+      render 'new'
+    end
   end
   
   private
   
   def picture_params
     params.require(:picture).permit(:title, :description)
+  end
+  
+  def find_picture
+    @picture = Picture.find(params[:id])
   end
   
 end
